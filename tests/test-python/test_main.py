27c284e97ec8b5919a8a2f7afcc4a05cf805db6e
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2026-05-22 16:38:27 +0200

    Release v0.0.8

--- tests/test-python/test_main.py
+++ tests/test-python/test_main.py
@@ -72,7 +72,7 @@ def _axes_geom_tooltips(gg: ggplot, geom_kind: str) -> dict:
 
 
 def test_version():
-    assert ninejs.__version__ == "0.0.7"
+    assert ninejs.__version__ == "0.0.8"
 
 
 def test_vector_to_list_accepts_common_iterables():
