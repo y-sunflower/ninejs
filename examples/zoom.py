6e383c6d74aa9aa5500c8c680da8dc57ead99c9b
JosephBARBIERDARNAL <JosephBARBIERDARNAL@users.noreply.github.com>
2026-06-12 16:03:07 +0000

    deploy: 0adb5def94f8c015ab17df629f425953485315a6

---
+++ examples/zoom.py
@@ -0,0 +1,15 @@
+from plotnine import ggplot, aes, geom_point, theme_minimal
+from plotnine.data import mpg
+
+from ninejs import interactive, save
+
+p = (
+    ggplot(
+        mpg,
+        aes(x="displ", y="hwy", color="drv", tooltip="model", data_id="drv"),
+    )
+    + geom_point(size=6, alpha=0.7)
+    + theme_minimal()
+)
+
+interactive(p, zoomable=True) + save("docs/iframes/zoom.html")
